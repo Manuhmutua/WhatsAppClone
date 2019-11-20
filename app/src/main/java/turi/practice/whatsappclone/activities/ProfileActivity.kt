cde832475a3eea55ec7df32d163ead8e9c5d63f1
washucode <estherw8525@gmail.com>
2019-11-20 12:04:18 +0300

    Profile Screen Functionality

--- app/src/main/java/turi/practice/whatsappclone/activities/ProfileActivity.kt
+++ app/src/main/java/turi/practice/whatsappclone/activities/ProfileActivity.kt
@@ -5,20 +5,82 @@ import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.firestore.FirebaseFirestore
+import kotlinx.android.synthetic.main.activity_profile.*
+import org.w3c.dom.Text
 import turi.practice.whatsappclone.R
+import turi.practice.whatsappclone.util.*
 
 class ProfileActivity : AppCompatActivity() {
-
+    private val firebaseDB = FirebaseFirestore.getInstance()
+    private val userId = FirebaseAuth.getInstance().currentUser?.uid
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+        if (userId.isNullOrEmpty()) {
+            finish()
+        }
+        progressLayout.setOnTouchListener { v, event -> true }
+        populateInfo()
     }
-    fun onApply(v: View){
 
+    private fun populateInfo() {
+        progressLayout.visibility = View.GONE
+        firebaseDB.collection(DATA_USERS).document(userId!!).get()
+            .addOnSuccessListener { documentSnapshot ->
+                val user = documentSnapshot.toObject(User::class.java)
+                nameET.setText(user?.name, TextView.BufferType.EDITABLE)
+                emailET.setText(user?.email, TextView.BufferType.EDITABLE)
+                phoneET.setText(user?.phone, TextView.BufferType.EDITABLE)
+                progressLayout.visibility = View.GONE
+            }
+            .addOnFailureListener { e ->
+                e.printStackTrace()
+                finish()
+            }
     }
-    fun onDelete    (v: View){
 
+    fun onApply(v: View) {
+        progressLayout.visibility = View.GONE
+        val name = nameET.text.toString()
+        val email = emailET.text.toString()
+        val phone = phoneET.text.toString()
+        val map = HashMap<String, Any>()
+        map[DATA_USER_NAME] = name
+        map[DATA_USER_EMAIL] = email
+        map[DATA_USER_PHONE] = phone
+        firebaseDB.collection(DATA_USERS).document(userId!!).update(map)
+            .addOnSuccessListener {
+                Toast.makeText(this, "Update successful", Toast.LENGTH_LONG).show()
+                finish()
+            }
+            .addOnFailureListener { e ->
+                e.printStackTrace()
+                Toast.makeText(this, "Update failed", Toast.LENGTH_LONG).show()
+                progressLayout.visibility = View.GONE
+            }
     }
+
+    fun onDelete(v: View) {
+        progressLayout.visibility = View.VISIBLE
+        AlertDialog.Builder(this)
+            .setTitle("Delete Account")
+            .setMessage("This will delete your profile information. Are you sure?")
+            .setPositiveButton("Yes") { dialog, which ->
+                Toast.makeText(this, "Profile deleted", Toast.LENGTH_LONG).show()
+                firebaseDB.collection(DATA_USERS).document(userId!!).delete()
+                finish()
+            }
+            .setNegativeButton("Cancel") { dialog, which ->
+                progressLayout.visibility = View.GONE
+            }
+            .show()
+    }
+
     companion object {
         fun newIntent(context: Context) = Intent(context, ProfileActivity::class.java)
     }
